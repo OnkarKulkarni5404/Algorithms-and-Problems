102ee3451fb0c4dc75c84b53ae9ee1a358846f95
Onkar Kulkarni <onkarkulkarni5404@gmail.com>
2018-10-26 18:23:48 +0530

    SPPU BE LP 1 Pract

---
+++ R Iris Pract/iris.r
@@ -0,0 +1,29 @@
+#get data from csv to dataframe
+data=read.csv("IRIS.csv")
+#get column names
+print(colnames(data))
+#count number of features
+ncol(data)
+#count number of rows
+nrow(data)
+#Types of features
+for(i in 1:ncol(data)){
+  print(colnames(data[i]))
+}
+#mean,max,avg,min,range,sd,variance,percentile
+for(i in 1:(ncol(data)-1))
+  {
+    print(colnames(data[i]))
+    min(data[,i])
+    max(data[,i])
+    sum(data[,i])/ncol(data)    
+    mean(data[,i])
+    sd(data[,i])
+    var(data[,i])
+    median(data[,i])
+    range(data[,i])
+    
+}
+
+#plotting histograms
+hist(data$petal_length)
